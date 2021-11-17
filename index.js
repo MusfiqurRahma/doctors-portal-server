275851d4dbeb81ebe74626ee016d7c6c77d4380f
Musfiqur Rahma <musfiq371@gmail.com>
2021-11-17 16:27:42 +0600

    'added_doctor'

--- index.js
+++ index.js
@@ -3,8 +3,11 @@ const { MongoClient } = require('mongodb');
 const cors = require('cors');
 const admin = require("firebase-admin");
 const { query } = require('express');
-const app = express()
-require('dotenv').config()
+const app = express();
+require('dotenv').config();
+const ObjectId = require('mongodb').ObjectId;
+const stripe = require('stripe')(process.env.STRIPE_SECRET);
+const fileUpload = require('express-fileupload');
 
 
 const port = process.env.PORT || 5000
@@ -13,6 +16,7 @@ const port = process.env.PORT || 5000
 // middleware
 app.use(cors());
 app.use(express.json())
+app.use(fileUpload());
 
 
 
@@ -48,6 +52,7 @@ async function run() {
       const database = client.db("doctors_portal");
       const appointmentCollection = database.collection("appointments");
       const userCollection = database.collection('users');
+      const doctorsCollection = database.collection('doctors');
       
       app.get('/appointments', async (req, res) => {
         const email = req.query.email;
@@ -56,30 +61,72 @@ async function run() {
         const cursor = appointmentCollection.find(query);
         const appointments = await cursor.toArray();
         res.json(appointments);
-      })
+      });
 
-      app.post('/appointments',verifyToken, async(req,res)=> {
+      app.post('/appointments', verifyToken, async (req, res) => {
         const appointment = req.body;
-        const result =await appointmentCollection.insertOne(appointment);
+        const result = await appointmentCollection.insertOne(appointment);
         res.json(result);
-      })
+      });
+
+      app.get('/appointments/:id', async (req, res) => {
+        const id = req.params.id;
+        const query = { _id: ObjectId(id) };
+        const result = await appointmentCollection.findOne(query);
+        res.json(result);
+      });
+
+      app.put('/appointments/:id', async (req, res) => {
+        const id = req.params.id;
+        const payment = req.body;
+        const filter = { _id: ObjectId(id) }
+        const updateDoc = {
+          $set: {
+            payment: payment
+          }
+        }
+        const result = await appointmentCollection.updateOne(filter, updateDoc);
+        res.json(result);
+      });
+      
+      app.get('/doctors', async (req, res) => {
+        const cursor = doctorsCollection.find({});
+        const doctors = await cursor.toArray();
+        res.json(doctors);
+      });
+
+      app.post('/doctors', async (req, res) => {
+        const name = req.body.name;
+        const email = req.body.email;
+        const pic = req.files.image;
+        const picData = pic.data;
+        const encodedPic = picData.toString('base64');
+        const imageBuffer = Buffer.from(encodedPic, 'base64');
+        const doctor = {
+          name,
+          email,
+          imageBuffer
+        }
+        const result =await doctorsCollection.insertOne(doctor)
+        res.json(result)
+      });
 
       app.get('/users/:email', async (req, res) => {
         const email = req.params.email;
         const query = { email: email }
         const user = await userCollection.findOne(query);
-        let isAdmin=false;
+        let isAdmin = false;
         if (user?.roll === 'admin') {
-          isAdmin =true;
+          isAdmin = true;
         }
-        res.json({ admin: isAdmin});
-      })
+        res.json({ admin: isAdmin });
+      });
 
       app.post('/users', async (req, res) => {
         const user = req.body;
         const result = await userCollection.insertOne(user);
         res.json(result);
-      })
+      });
 
       app.put('/users', async (req, res) => {
         const user = req.body;
@@ -89,7 +136,7 @@ async function run() {
         const result = await userCollection.updateOne(filter, updateDoc, options);
         res.json(result);
     
-      })
+      });
 
       app.put('/users/admin',verifyToken, async (req, res) => {
         const user = req.body;
@@ -111,6 +158,18 @@ async function run() {
       }
         
       })
+
+      app.post('/create-payment-intent', async (req, res) => {
+        const paymentInfo = req.body;
+        const amount = paymentInfo.price * 100;
+        const paymentIntent = await stripe.paymentIntents.create({
+            currency: 'usd',
+            amount: amount,
+            payment_method_types: ['card']
+        });
+        res.json({ clientSecret: paymentIntent.client_secret })
+    })
+
     }
     finally {
       // await client.close();
@@ -120,8 +179,8 @@ async function run() {
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
-})
+});
 
 app.listen(port, () => {
   console.log(` listening at ${port}`)
-})
+});
